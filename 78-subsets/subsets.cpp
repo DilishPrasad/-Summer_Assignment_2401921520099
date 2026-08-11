f5f6ec9c8550967c6907f39f6aef3ee82aa4e5bb
DilishPrasad <dilishprasad2020@gmail.com>
2026-08-11 11:06:49 +0530

    Time: 4 ms (7.58%) | Memory: 15.7 MB (11.10%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+      void subseq(vector<int>&arr,int index,int n,vector<vector<int > >&ans,vector<int>temp)
+      {
+        if(index==n)
+        {
+            ans.push_back(temp);
+            return;
+
+        }
+        //not included
+        subseq(arr,index+1,n,ans,temp);
+        //included
+        temp.push_back(arr[index]);
+        subseq(arr,index+1,n,ans,temp);
+      }
+
+
+
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<vector<int> >ans;
+        vector<int>temp;
+        subseq(arr,0,arr.size(),ans,temp);
+        return ans;
+        
+    }
+};
